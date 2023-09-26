8edf85fc3ff8defd2b1e913f6cf48469254def0d
nidhijoshi14 <93935753+nidhijoshi14@users.noreply.github.com>
2023-09-26 13:50:20 +0530

    Update

---
+++ lib/Utils/List/VideoList/videolist.dart
@@ -0,0 +1,18 @@
+List<Map<String, dynamic>> listvideo = [
+  {
+    'video':
+        "lib/Utils/Assets/Video/KhanSir'sTeachingTechniquesImpressesAB.mp4",
+    'vtitle': "Khan Sir On KBC",
+    'vimage': "lib/Utils/Assets/VideoPhotos/terehavale.jpg",
+  },
+  {
+    'video': "lib/Utils/Assets/Video/videoplayback.mp4",
+    'vtitle': "Desh Mere",
+    'vimage': "lib/Utils/Assets/VideoPhotos/terehavale.jpg",
+  },
+  {
+    'video': "lib/Utils/Assets/Video/videoplayback(1).mp4",
+    'vtitle': "Tere Hawaale",
+    'vimage': "lib/Utils/Assets/VideoPhotos/terehavale.jpg",
+  },
+];
